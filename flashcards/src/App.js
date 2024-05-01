ef75fefe54db20a380e3649cd3197774eb06c53a
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2024-05-01 21:52:35 +0200

    examples added

--- flashcards/src/App.js
+++ flashcards/src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [cards, setCards] = useState([]);
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
   const [flipped, setFlipped] = useState(false);
+  const [example, setExample] = useState('');
 
   useEffect(() => {
     fetch('/flashcards.json')
@@ -22,6 +23,7 @@ function App() {
   const handleNextCard = () => {
     setFlipped(false);
     setCurrentCardIndex((prevIndex) => (prevIndex + 1) % cards.length);
+    setExample('');
   };
 
   const handlePrevCard = () => {
@@ -29,6 +31,7 @@ function App() {
     setCurrentCardIndex((prevIndex) =>
       prevIndex === 0 ? cards.length - 1 : prevIndex - 1
     );
+    setExample('');
   };
 
   const handleFlipCard = () => {
@@ -52,9 +55,30 @@ function App() {
     };
   });
 
+  const handleGetExample = (word) => {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then((response) => response.json())
+      .then((data) => {
+        let exampleUsage = 'No example found';
+        for (const meaning of data[0].meanings) {
+          if (meaning.definitions && meaning.definitions.length > 0 && meaning.definitions[0].example) {
+            exampleUsage = meaning.definitions[0].example;
+            break;
+          }
+        }
+        setExample(exampleUsage);
+      })
+      .catch((error) => {
+        console.error(`Error fetching example for ${word}:`, error);
+        setExample('Error fetching example');
+      });
+  };
+
   return (
     <div className="app">
-      <h1 className="title">Czas na naukę... 📚</h1>
+      <div className="title-wrapper">
+        <h1 className="title">Czas na naukę... 📚</h1>
+      </div>
 
       <div className="flashcard-container">
         {cards.length > 0 && (
@@ -73,21 +97,32 @@ function App() {
           </div>
         )}
       </div>
-      {cards.length > 0 && (
-        <div className="buttons">
-          <div className="button-wrapper">
-            <button className="prev-button" onClick={handlePrevCard}>
-              <FontAwesomeIcon icon={faArrowLeft} />
-            </button>
-            <div className="flashcard-info">
-              {`${currentCardIndex + 1}/${cards.length}`}
-            </div>
-            <button className="next-button" onClick={handleNextCard}>
-              <FontAwesomeIcon icon={faArrowRight} />
-            </button>
+
+      <div className="buttons">
+        <div className="button-wrapper">
+          <button className="prev-button" onClick={handlePrevCard}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <div className="flashcard-info">
+            {`${currentCardIndex + 1}/${cards.length}`}
           </div>
+          <button className="next-button" onClick={handleNextCard}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
         </div>
-      )}
+      </div>
+
+      <div className="example-wrapper">
+        <button className="example-button" onClick={() => handleGetExample(cards[currentCardIndex].front)}>
+          <FontAwesomeIcon icon={faArrowRight} />
+          <span>Get example </span>
+        </button>
+        {example && (
+          <div className="example">
+            <p>{example}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
